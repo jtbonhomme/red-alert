b03cec320004060fd3bbfc0df9dab5709462aa08
Jean-Thierry BONHOMME <jtbonhomme@gmail.com>
2018-02-07 15:02:15 +0100

    Added Device control view

---
+++ RedAlert/ConnectViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ConnectViewController.swift
+//  RedAlert
+//
+//  Created by Jean-Thierry BONHOMME on 06/02/2018.
+//  Copyright © 2018 Jean-Thierry BONHOMME. All rights reserved.
+//
+
+import UIKit
+
+class ConnectViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
